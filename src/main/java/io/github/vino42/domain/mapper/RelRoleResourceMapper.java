7353b66b961259592a37cdc8f329bbc2db8dd65f
wangbingya <38912428@qq.com>
2021-11-20 16:28:26 +0800

    commit rbac 基于简单文件配置的rbac 角色资源访问控制

---
+++ src/main/java/io/github/vino42/domain/mapper/RelRoleResourceMapper.java
@@ -0,0 +1,21 @@
+package io.github.vino42.domain.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import io.github.vino42.domain.entity.RelRoleResourceEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * =====================================================================================
+ *
+ * @Created :   2021/11/20 15:04:28
+ * @Compiler :  jdk 11
+ * @Author :    VINO
+ * @Email : 38912428@qq.com
+ * @Copyright : VINO
+ * @Decription :  Mapper 接口
+ * =====================================================================================
+ */
+@Mapper
+public interface RelRoleResourceMapper extends BaseMapper<RelRoleResourceEntity> {
+
+}
